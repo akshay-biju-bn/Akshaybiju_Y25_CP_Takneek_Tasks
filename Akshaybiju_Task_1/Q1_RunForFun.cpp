1feeef04736b1e6b2283779640a93633e57a6e1a
akshay-biju-bn <akshaybijubn@gmail.com>
2025-08-10 13:01:54 +0530

    Create Q1_RunForFun.cpp

---
+++ Akshaybiju_Task_1/Q1_RunForFun.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int X,Y;
+	cin>>X>>Y;
+	if(Y%X==0)
+	    {
+	        cout<<(Y/X)-1;
+	        
+	    }
+	else
+	    {
+	        cout<<(Y/X);
+	        
+	    }
+
+}
